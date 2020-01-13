94f9b7596f4a12b6b3985d8076036342340b507e
TheRanger22 <schandingl1@nku.edu>
2020-01-13 15:32:16 -0500

    laid framework

--- index.php
+++ index.php
@@ -7,8 +7,11 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/custom.css">
     <title>Hello, world!</title>
 </head>
 <body>
@@ -20,6 +23,23 @@ $uMajor = "Computer Science";
 $uYear = "Junior";
 $uImg = "";
 ?>
+
+<div class="container-fluid" id="animate-area">
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-11" >
+            <div class="row scs-bg-hdr">
+                <?php echo $name?>
+            </div>
+            <div class="row cstm-border standard-container">
+                <div class="col">
+                    <?php echo $email ?>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+</div>
 
 </body>
 </html>
