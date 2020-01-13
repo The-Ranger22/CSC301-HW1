324787e7f37744c6743e1fbdde56c99021fe0ae7
TheRanger22 <schandingl1@nku.edu>
2020-01-13 14:07:38 -0500

    second commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!--Student: Levi Schanding-->
+<!--Assignment 1-->
+<!--Personal note: I've been looking forward to this, PHP looks fun.-->
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Hello, world!</title>
+</head>
+<body>
+<?php
+//Variables
+$name = "Levi Schanding";
+$email = "schandingl1@nku.edu";
+$uMajor = "Computer Science";
+$uYear = "Junior";
+$uImg = "";
+?>
+
+</body>
+</html>
