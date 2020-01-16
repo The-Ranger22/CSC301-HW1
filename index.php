79de7e77397cb8374e6b6ab4c2de694ba68373de
TheRanger22 <schandingl1@nku.edu>
2020-01-16 12:04:54 -0500

    end of class commit

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <!--Student: Levi Schanding-->
 <!--Assignment 1-->
 <!--Personal note: I've been looking forward to this, PHP looks fun.-->
+<?php
+//Echo Vs Print: They're the same to the point that the differences between them are negligible.
+//Variables
+$name = "Levi Schanding";
+$email = "schandingl1@nku.edu";
+$uMajor = "Computer Science";
+$uYear = "Junior";
+$uImg = "img/dummy_pic.png";
+$bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec ornare enim. Vestibulum ut tortor eget magna vehicula ultricies eu non tellus. Nunc cursus malesuada justo, eget egestas enim cursus eu. Nunc sed neque in eros tincidunt lacinia sed sed tortor. Quisque aliquam semper dignissim. Vivamus gravida consectetur dolor ut sollicitudin. Aenean aliquet euismod ipsum, id sollicitudin sem pulvinar in. Ut tempor massa vel risus pretium dapibus. Praesent at nulla nec est dictum finibus.";
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,39 +25,39 @@
     <title>Hello, world!</title>
 </head>
 <body>
-<?php
-//Variables
-$name = "Levi Schanding";
-$email = "schandingl1@nku.edu";
-$uMajor = "Computer Science";
-$uYear = "Junior";
-$uImg = "img/dummy_pic.png";
-$bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec ornare enim. Vestibulum ut tortor eget magna vehicula ultricies eu non tellus. Nunc cursus malesuada justo, eget egestas enim cursus eu. Nunc sed neque in eros tincidunt lacinia sed sed tortor. Quisque aliquam semper dignissim. Vivamus gravida consectetur dolor ut sollicitudin. Aenean aliquet euismod ipsum, id sollicitudin sem pulvinar in. Ut tempor massa vel risus pretium dapibus. Praesent at nulla nec est dictum finibus.";
-?>
+
 
 <div class="container-fluid" id="animate-area">
     <div class="row">
         <div class="col"></div>
-        <div class="col-11" >
+        <div class="col-11">
             <div class="row scs-bg-hdr">
-                <?php echo $name?>
+                <?= $name ?>
             </div>
             <div class="row cstm-border standard-container">
                 <div class="col">
                     <div class="row">
-                        <div class="col"></div>
-                        <img class="cstm-border col-4 p-null m-null" src="<?php echo $uImg ?>"/>
-                        <div class="col"></div>
+                        <div class="col-8">
+                            <div class="row header-text">
+                                <div class="col">Major: <?= $uMajor ?></div>
+                                <div class="col">Year: <?= $uYear ?></div>
+                            </div>
+                        </div>
+                        <img class="cstm-border col-4 p-null m-null" src="<?= $uImg ?>"/>
                     </div>
                     <div class="spacer2"></div>
                     <div class="row">
-                        <a class="col header-text text-center" href="mailto:<?php echo $email ?>">Email me here!</a>
+                        <a class="col header-text text-center" href="mailto:<?= $email ?>">Email me here!</a>
                     </div>
-                    <div class="row">
-                        <h3 class="col header-text">Bio:</h3>
-                    </div>
-                    <div class="row">
-                        <p class="col text-center"><?php echo $bio ?></p>
+                    <div class="row standard-container cstm-border">
+                        <div class="col">
+                            <div class="row">
+                                <h4 class="col header-text">Bio:</h4>
+                            </div>
+                            <div class="row">
+                                <p class="col text-center"><?= $bio ?></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
